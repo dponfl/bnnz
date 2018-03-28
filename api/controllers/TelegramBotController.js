fb9082af614ad4429374b981332733b00821bbaa
dmsch <dschukin@mail.ru>
2018-03-28 21:53:44 +0300

    test message

--- api/controllers/TelegramBotController.js
+++ api/controllers/TelegramBotController.js
@@ -5,13 +5,21 @@ const TelegramBot = require('node-telegram-bot-api');
 module.exports = {
   getResponse: function (req, res) {
 
+    var result = {};
+
     console.log('<== TelegramBotController:getResponse ==>');
 
-    console.log('Telegram Token: ' + sails.config.TOKEN);
+    // console.log('Telegram Token: ' + sails.config.TOKEN);
 
-    let result = {
-      some: 'some text',
-    };
+    const bot = new TelegramBot(sails.config.TOKEN, {
+      polling: true
+    });
+
+    bot.on('message', (msg) => {
+      console.log('Bot got message: ' + msg);
+      result['msg'] = msg;
+    });
+
 
     return res.ok({result: 'ok', data: result});
 
